7af9d2b7f4086351fedfd20d4f48433f6a23673d
jotaro4u <kothaker135@gmail.com>
2024-11-18 23:23:42 +0300

    2

--- Program.cs
+++ Program.cs
@@ -8,22 +8,52 @@ namespace ConsoleApp_9
 {
     internal class Program
     {
-        // Задание 1
-        public static void Sayhello()
+        // Задание 6
+        public static void PER()
         {
-            Console.WriteLine("привет");
+            Console.Write("x1 = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("x2 = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            int y2 = int.Parse(Console.ReadLine());
+            Console.Write("x3 = ");
+            int x3 = int.Parse(Console.ReadLine());
+            Console.Write("y3 = ");
+            int y3 = int.Parse(Console.ReadLine());
+
+            double side1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double side2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            double side3 = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+
+            Console.WriteLine($"Периметр треугольника: {side1 + side2 + side3}");
         }
 
-        public static void Saypoka()
+        public static void S()
         {
-            Console.WriteLine("пока");
+            Console.Write("x1 = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("x2 = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            int y2 = int.Parse(Console.ReadLine());
+            Console.Write("x3 = ");
+            int x3 = int.Parse(Console.ReadLine());
+            Console.Write("y3 = ");
+            int y3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Площадь треугольника: {0.5 * Math.Abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))}");
         }
 
         static void Main(string[] args)
         {
-            // Задание 1
-            Sayhello();
-            Saypoka();
+            // Задание 6
+            PER();
+            S();
 
             Console.Read();
         }
