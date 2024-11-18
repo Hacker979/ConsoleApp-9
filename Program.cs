c2a6d7d19816d65dea997d090c307b4f6fa8e47f
jotaro4u <kothaker135@gmail.com>
2024-11-18 23:23:00 +0300

    2

--- Program.cs
+++ Program.cs
@@ -8,22 +8,38 @@ namespace ConsoleApp_9
 {
     internal class Program
     {
-        // Задание 1
-        public static void Sayhello()
+        // Задание 5
+        public static void F()
         {
-            Console.WriteLine("привет");
+            Console.Write("x1 = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("x2 = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            int y2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Длина отрезка: {Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2))}");
         }
 
-        public static void Saypoka()
+        public static void D()
         {
-            Console.WriteLine("пока");
+            Console.Write("Введите длину стороны a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Периметр треугольника: {a + b + c}");
         }
 
         static void Main(string[] args)
         {
-            // Задание 1
-            Sayhello();
-            Saypoka();
+            // Задание 5
+            F();
+            D();
 
             Console.Read();
         }
