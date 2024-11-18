39bf34f7555bf246e2f092ce6ad586a32910eb17
jotaro4u <kothaker135@gmail.com>
2024-11-18 23:21:55 +0300

    2

--- Program.cs
+++ Program.cs
@@ -8,22 +8,21 @@ namespace ConsoleApp_9
 {
     internal class Program
     {
-        // Задание 1
-        public static void Sayhello()
+        // Задание 3
+        public static void fun()
         {
-            Console.WriteLine("привет");
-        }
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
 
-        public static void Saypoka()
-        {
-            Console.WriteLine("пока");
+            Console.WriteLine(Math.Pow(12 * a, 2) + 7 * a - 6);
         }
 
         static void Main(string[] args)
         {
-            // Задание 1
-            Sayhello();
-            Saypoka();
+            // Задание 3
+            fun();
 
             Console.Read();
         }
