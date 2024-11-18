24673124eb2bb5e3e77354b6819ea75cced1789b
jotaro4u <kothaker135@gmail.com>
2024-11-18 22:26:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_9
+{
+    internal class Program
+    {
+        // Задание 1
+        public static void Sayhello()
+        {
+            Console.WriteLine("привет");
+        }
+
+        public static void Saypoka()
+        {
+            Console.WriteLine("пока");
+        }
+
+        static void Main(string[] args)
+        {
+            // Задание 1
+            Sayhello();
+            Saypoka();
+
+            Console.Read();
+        }
+    }
+}
